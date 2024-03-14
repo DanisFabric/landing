6adeac36b19b2c303a6d8a0a34abefd27f0a3870
danis <danisfabric@gmail.com>
2024-03-14 12:02:12 +0800

    add menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {logo, lock} from '../assets'
 
 const NavBar = () => {
   return (
-    <div className='bg-black text-white'>
-      Danis
+    <div className='border-b h-[80px] w-full'>
+      <div className='m-auto max-w-[1200px] h-full flex justify-between items-center bg-slate-200'>
+        <img src={logo} className='h-6'/>
+        <div className>
+          <ul className='flex gap-4'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platform</li>
+            <li>Pricing</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
