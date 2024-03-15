31c6f49d1dd40894616bd97146c749705e5f1644
danis <danisfabric@gmail.com>
2024-03-15 16:34:06 +0800

    finish courses

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { blogImg1 } from "../assets";
 import StarRating from "./StarRating";
 
-const Card = () => {
+const Card = ({course}) => {
   return (
-    <div className="bg-white drop-shadow-md rounded-2xl overflow-hidden">
-      <img src={blogImg1} className="h-40 w-full object-cover" />
+    <div className="bg-white drop-shadow-md rounded-2xl overflow-hidden mr-4 my-2">
+      <img src={course.linkImg} className="h-40 w-full object-cover" />
       <div className="p-5 border-b border">
-        <h1 className="py-2">Python Programming</h1>
-        <StarRating rating={4} />
+        <h1 className="py-2 truncate">{course.title}</h1>
+        <StarRating rating={course.rating} />
       </div>
-      <h3 className="p-5 text-xl">$500</h3>
+      <h3 className="p-5 text-xl">{course.price}</h3>
       <div className="absolute top-0 bg-white m-3 px-2 py-[2.5px] rounded font-bold">
-        HTML
+        {course.category}
       </div>
     </div>
   );
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import Card from "./Card";
 import Slider from "react-slick";
+import { courses } from "../data/Courses";
 
 const Courses = () => {
   var settings = {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      }
+    ]
   }
   return (
     <div className="w-full bg-[#E9F8F3B2] py-[50px]">
-      <div className="max-w-[1480px] m-auto p-5">
+      <div className="max-w-[1480px] m-auto p-10">
+        <h1 className="text-3xl font-bold py-5">Most Popular<span className="text-[#20B486]">Courses</span></h1>
+        <p className="text-[#6D737A] font-thin mb-4">Various versions have evolved over the years, sometimes by accident,</p>
         <Slider {...settings}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          { courses.map((course) => {
+            return (
+              <Card course={course} />
+            );
+          }) }
         </Slider>
       </div>
     </div>
