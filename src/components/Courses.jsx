2a91042a2dd42c644eb39525875c6945d479bc61
danis <danisfabric@gmail.com>
2024-03-15 12:13:05 +0800

    add slider

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import Slider from "react-slick";
 
 const Courses = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  }
   return (
-    <div className='w-full bg-[#E9F8F3B2] py-[50px]'>
-        <div className='max-w-[1480px] m-auto p-5'>
-            <Card />
-            <Card />
-        </div>
+    <div className="w-full bg-[#E9F8F3B2] py-[50px]">
+      <div className="max-w-[1480px] m-auto p-5">
+        <Slider {...settings}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Slider>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Courses
+export default Courses;
